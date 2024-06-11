beb2178b92e7d181111215d9f275c852e3bb7ab1
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-06-11 18:57:56 +0530

    Added Logout

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import { BottomWarning } from "../components/BottomWarning";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Signin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading title={"Sign In"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox
+            placeholder={"abc123@example.com"}
+            label={"Email"}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder={"Your Strong Password"}
+            label={"Password"}
+            type={"password"}
+          />
+          <div className="pt-4">
+            <Button
+              label={"Sign In"}
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:5000/api/v1/user/signin",
+                  {
+                    username,
+                    password,
+                  }
+                );
+                if (response.data.success) {
+                  localStorage.setItem("token", response.data.token);
+                  navigate("/dashboard");
+                }
+              }}
+            />
+          </div>
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign Up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,80 @@
+import { BottomWarning } from "../components/BottomWarning";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading title={"Sign Up"} />
+          <SubHeading label={"Enter your details to create an account"} />
+          <InputBox
+            placeholder={"John"}
+            label={"First Name"}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+          <InputBox
+            placeholder={"Doe"}
+            label={"Last Name"}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
+          <InputBox
+            placeholder={"abc123@example.com"}
+            label={"Email"}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <InputBox
+            placeholder={"Some Strong Password"}
+            label={"Password"}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type={"password"}
+          />
+          <div className="pt-4">
+            <Button
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:5000/api/v1/user/signup",
+                  {
+                    username,
+                    firstName,
+                    lastName,
+                    password,
+                  }
+                );
+                localStorage.setItem("token", response.data.token);
+                navigate("/dashboard");
+              }}
+              label={"Sign Up"}
+            />
+          </div>
+          <BottomWarning
+            label={"Already have an account?"}
+            buttonText={"Sign In"}
+            to={"/signin"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;

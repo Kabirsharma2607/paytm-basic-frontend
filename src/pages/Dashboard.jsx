d41d1bef2a631b0c2ecf71e845877f0a228eb844
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-06-12 10:41:17 +0530

    updated Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "../components/AppBar";
 import Balance from "../components/Balance";
 import Users from "../components/Users";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Dashboard() {
+  const [balance, setBalance] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
-      navigate("/signup");
+      navigate("/");
+    }
+    try {
+      const fetchData = async () => {
+        const res = await axios.get(
+          "https://paytm-basic-backend-i8ru.onrender.com/api/v1/account/balance",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        console.log(res?.data?.balance);
+        setBalance(res?.data?.balance);
+      };
+
+      fetchData();
+    } catch (error) {
+      console.log(error.message);
     }
   }, []);
   return (
     <div>
       <AppBar />
       <div className="m-8">
-        <Balance value={"10,000"} />
+        <Balance value={balance} />
         <Users />
       </div>
     </div>
